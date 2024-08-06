d4d62bdca33cb9acd768a48f038201c582d8c6a6
Marcelo Torres <kevin.torres85@outlook.es>
2024-08-05 20:06:12 -0500

    clase05-08-2024

---
+++ Clase05-08-2024/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase05_08_2024
+{
+    public class Persona
+    {
+        public string nombre;
+        public int edad;
+
+        public string Nombre
+        {
+            //get para recuperar un valor
+            get { return nombre; }
+
+            //ser para establecer un valor
+            set { nombre = value; }
+
+            //crear 2 atributos
+        }
+
+        private int num1;
+        private int num2;
+
+        public int Num1
+        {
+            get { return num1; }
+            set { num1 = value; }
+        }
+
+        public int Num2
+        {
+            get { return num2; }
+            set { num2 = value; }
+        }
+    }
+}
---
+++ Clase05-08-2024/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase05_08_2024
+{
+    internal class Program
+    {
+        /*Que es la herencia?
+         * la herencia me permite crear clases nuevas a opartirt de una clse base. 
+         * A demás, me permite heredar atributos y métodos.
+         *
+         *
+         *Modificador de Acceos
+         *Private: Cuando los atributos y métodos están marcados con el modificador de acceso privado 
+         *solo puede ser accedidio desde la propia clase que la declara
+         *
+         
+         
+         */
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            //Establecer valor
+            persona.Nombre = "Marcelo";
+            //Valor recuperado
+            string nombre = persona.Nombre;
+
+            persona.Num1 = 5;
+            persona.Num2 = 16;
+
+            int suma = persona.Num1 + persona.Num2;
+
+            Console.WriteLine($"La suma es: {suma}");
+            Console.ReadKey();
+
+        }
+    }
+}
